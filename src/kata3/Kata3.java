c1f970b6f760acbb8abcd2ccf42c60582a225099
raulm <raulm@DESKTOP-AUJH7VA>
2021-11-17 17:35:47 +0000

    Kata3 versión 2

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -3,9 +3,15 @@ package kata3;
 public class Kata3 {
 
     public static void main(String[] args) {
-        
-        HistogramDisplay histo = new HistogramDisplay();
-        histo.execute();
+        Histogram<String> histogram = new Histogram<String>();
+        histogram.increment("gmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        new HistogramDisplay(histogram).execute();
     }
     
 }
